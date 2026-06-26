f0015da0beabf4bcf91e623ba7cae08de0355102
chrjl <73675826+chrjl@users.noreply.github.com>
2026-06-26 08:34:16 -0700

    test(integration): green coffee create, update

--- tests/test_write_integration/conftest.py
+++ tests/test_write_integration/conftest.py
@@ -2,6 +2,7 @@ import pytest
 import json
 
 from bin.seed.seed_roasters import sample_roaster_data
+from bin.seed.seed_green_coffees import sample_green_coffee_data
 
 
 @pytest.fixture
@@ -67,3 +68,104 @@ def seed_sample_roasters(roasters_list, create_roaster):
         roaster_ids.append(new_roaster["id"])
 
     return roaster_ids
+
+
+@pytest.fixture
+def origin_id(client):
+    def _origin_id(origin_name: str) -> int | None:
+        query = """
+            query($filter: Filter) {
+                origins(filter: $filter) {
+                    name
+                    id
+                }
+            }
+        """
+
+        variables = {"filter": {"name": {"starts_with": origin_name}}}
+
+        response = client.post("/", json={"query": query, "variables": variables})
+        result = response.json()["data"]["origins"]
+
+        if len(result) == 1:
+            return result[0]["id"]
+
+        return None
+
+    return _origin_id
+
+
+@pytest.fixture
+def green_coffees_list():
+    return sample_green_coffee_data()
+
+
+@pytest.fixture
+def create_green_coffee(client, origin_id):
+    def _create_green_coffee(green_coffee):
+        input = {}
+
+        for v in ["name", "processes", "varieties", "tasting"]:
+            if v in green_coffee:
+                input[v] = green_coffee[v]
+
+        if any(
+            [
+                v in green_coffee
+                for v in ["source", "source_type", "community", "origin_name"]
+            ]
+        ):
+            input["source"] = {}
+
+            if "source" in green_coffee:
+                input["source"]["name"] = green_coffee["source"]
+            if "source_type" in green_coffee:
+                input["source"]["type"] = green_coffee["source_type"]
+            if "community" in green_coffee:
+                input["source"]["community"] = green_coffee["community"]
+            if "origin_name" in green_coffee:
+                input["source"]["originId"] = origin_id(green_coffee["origin_name"])
+
+        if "details" in green_coffee:
+            input["details"] = json.dumps(green_coffee["details"])
+
+        query = """
+        mutation($input: GreenCoffeeInput!) {
+            greenCoffeeCreate(input: $input) {
+                status
+                error {
+                    code
+                    message
+                }
+                greenCoffee {
+                    id
+                }
+            }
+        }
+        """
+
+        response = client.post(
+            "/", json={"query": query, "variables": {"input": input}}
+        )
+
+        assert response.status_code == 200
+
+        result = response.json()["data"]["greenCoffeeCreate"]
+
+        if result["status"] == True:
+            return result["greenCoffee"]
+        else:
+            return result["error"]
+
+    return _create_green_coffee
+
+
+@pytest.fixture
+def seed_sample_green_coffees(green_coffees_list, create_green_coffee):
+    green_coffee_ids = []
+
+    for coffee in green_coffees_list:
+        new_green_coffee = create_green_coffee(coffee)
+        green_coffee_ids.append(new_green_coffee["id"])
+
+    return green_coffee_ids
