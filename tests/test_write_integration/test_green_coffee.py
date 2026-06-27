4f5c9655452bdfcf9083788758f466dd0cbc4205
chrjl <73675826+chrjl@users.noreply.github.com>
2026-06-26 17:14:43 -0700

    test(integration): green coffee tags

--- tests/test_write_integration/test_green_coffee.py
+++ tests/test_write_integration/test_green_coffee.py
@@ -94,3 +94,159 @@ def test_update_green_coffee(
     assert result["name"] == name
     assert set(result["varieties"]) == set(sample_data["varieties"])
     assert set(result["processes"]) == set(new_processes)
+
+
+def test_green_coffee_tag_add(client, green_coffees_list, create_green_coffee):
+    sample_green_coffee = green_coffees_list[0]
+    green_coffee_obj = create_green_coffee(sample_green_coffee)
+
+    mutation_query = """
+    mutation($id: ID!, $type: String!, $values: [String]!) {
+        greenCoffeeTagAdd(id: $id, type: $type, values: $values) {
+            status
+            error {
+                code
+                message
+            }
+            greenCoffee {
+                id
+                name
+                processes
+                varieties
+                tasting
+            }
+        }
+    }
+    """
+
+    response = client.post(
+        "/",
+        json={
+            "query": mutation_query,
+            "variables": {
+                "id": green_coffee_obj["id"],
+                "type": "process",
+                "values": ["test_value"],
+            },
+        },
+    )
+    assert response.status_code == 200
+
+    result = response.json()["data"]["greenCoffeeTagAdd"]["greenCoffee"]
+
+    assert len(result["processes"]) == len(sample_green_coffee["processes"]) + 1
+    assert set(result["processes"]) == set(
+        [*sample_green_coffee["processes"], "test_value"]
+    )
+    assert result["varieties"] == sample_green_coffee["varieties"]
+    assert result["tasting"] == sample_green_coffee["tasting"]
+
+
+def test_green_coffee_tag_clear(client, green_coffees_list, create_green_coffee):
+    sample_green_coffee = green_coffees_list[0]
+    green_coffee_obj = create_green_coffee(sample_green_coffee)
+
+    mutation_query = """
+    mutation($id: ID!, $type: String!) {
+        greenCoffeeTagDelete(id: $id, type: $type) {
+            status
+            error {
+                code
+                message
+            }
+            greenCoffee {
+                id
+                name
+                processes
+                varieties
+                tasting
+            }
+        }
+    }
+    """
+
+    response = client.post(
+        "/",
+        json={
+            "query": mutation_query,
+            "variables": {"id": green_coffee_obj["id"], "type": "process"},
+        },
+    )
+    assert response.status_code == 200
+
+    result = response.json()["data"]["greenCoffeeTagDelete"]["greenCoffee"]
+
+    assert result["processes"] == []
+    assert len(result["varieties"]) == len(sample_green_coffee["varieties"])
+    assert len(result["tasting"]) == len(sample_green_coffee["tasting"])
+
+    response = client.post(
+        "/",
+        json={
+            "query": mutation_query,
+            "variables": {"id": green_coffee_obj["id"], "type": "variety"},
+        },
+    )
+    assert response.status_code == 200
+
+    result = response.json()["data"]["greenCoffeeTagDelete"]["greenCoffee"]
+
+    assert result["processes"] == []
+    assert result["varieties"] == []
+    assert len(result["tasting"]) == len(sample_green_coffee["tasting"])
+
+    response = client.post(
+        "/",
+        json={
+            "query": mutation_query,
+            "variables": {"id": green_coffee_obj["id"], "type": "error"},
+        },
+    )
+    assert response.status_code == 200
+
+    result = response.json()["data"]["greenCoffeeTagDelete"]["error"]
+
+    assert result["code"] == 400
+
+
+def test_green_coffee_tag_delete(client, green_coffees_list, create_green_coffee):
+    sample_green_coffee = green_coffees_list[0]
+    green_coffee_obj = create_green_coffee(sample_green_coffee)
+
+    mutation_query = """
+    mutation($id: ID!, $type: String!, $values: [String]) {
+        greenCoffeeTagDelete(id: $id, type: $type, values: $values) {
+            status
+            error {
+                code
+                message
+            }
+            greenCoffee {
+                id
+                name
+                processes
+                varieties
+                tasting
+            }
+        }
+    }
+    """
+
+    response = client.post(
+        "/",
+        json={
+            "query": mutation_query,
+            "variables": {
+                "id": green_coffee_obj["id"],
+                "type": "variety",
+                "values": [sample_green_coffee["varieties"][-1]],
+            },
+        },
+    )
+    assert response.status_code == 200
+
+    result = response.json()["data"]["greenCoffeeTagDelete"]["greenCoffee"]
+
+    assert result["processes"] == sample_green_coffee["processes"]
+    assert result["tasting"] == sample_green_coffee["tasting"]
+    assert result["varieties"] == sample_green_coffee["varieties"][:-1]
