03cfa399e3359756831bb77f613067cb11a70d81
chrjl <73675826+chrjl@users.noreply.github.com>
2026-07-01 14:24:34 -0700

    refactor(tests): seed sample data

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,10 @@ from sqlalchemy.pool import StaticPool
 from db import models
 from api.main import app
 from bin.seed_countries_regions import generate_country_objects, generate_origin_objects
-from bin.seed_sample_data import green_coffee_objects, roaster_object_and_associations
+
+from bin.seed.seed_roasters import sample_roaster_objects
+from bin.seed.seed_green_coffees import sample_green_coffee_objects
+from bin.seed.seed_roasted_coffees import sample_roasted_coffee_objects
 
 SAMPLE_DATA_PATH = "assets/sample-data.json"
 
@@ -28,28 +31,13 @@ def engine(request):
     echo_marker = request.node.get_closest_marker("echo")
 
     def seed_sample_data(engine):
-        with open(SAMPLE_DATA_PATH) as file:
-            data = json.load(file)
-
-        # Seed sample data
         with Session(engine) as session:
-            if green_coffee_data := data.get("green_coffees"):
-                session.add_all(green_coffee_objects(engine, green_coffee_data))
-                session.commit()
+            session.add_all(sample_roaster_objects())
+            session.add_all(sample_green_coffee_objects(engine))
+            session.commit()
 
-        for roaster_data in data.get("roasters"):
-            with Session(engine) as session:
-                r = roaster_object_and_associations(engine, roaster_data)
-
-                roaster_object = r.get("roaster")
-                session.add(roaster_object)
-
-                coffee_objects = r.get("coffees", [])
-                session.add_all(coffee_objects)
-
-                component_objects = r.get("component_associations", [])
-                session.add_all(component_objects)
-                session.commit()
+            session.add_all(sample_roasted_coffee_objects(engine))
+            session.commit()
 
     echo = echo_marker and (echo_marker.args[0] is True)
     engine = create_engine(
